68c2a8d489c72344aad627f0664860722c6f14f9
sunJ0120 <sspure1214@gmail.com>
2025-12-12 14:06:49 +0900

    feat: security 설정을 위해 필요한 기본 user 파일 생성

--- be/src/main/java/com/example/be/user/Role.java
+++ be/src/main/java/com/example/be/user/Role.java
@@ -1,4 +1,13 @@
 package com.example.be.user;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
+    USER("USER"),
+    ADMIN("ADMIN");
+
+    private final String role;
 }
--- be/src/main/java/com/example/be/user/User.java
+++ be/src/main/java/com/example/be/user/User.java
@@ -1,4 +1,45 @@
 package com.example.be.user;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)    // 생성/수정 시간 자동 관리를 위해 추가
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = true)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String phone;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime  createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
