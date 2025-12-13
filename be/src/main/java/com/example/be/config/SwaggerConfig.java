423a8109bbe7763c797aed98a8e9034d5041370e
sunJ0120 <sspure1214@gmail.com>
2025-12-13 22:03:52 +0900

    feat: 도메인별로 나눠서 swagger 설정 구성

--- be/src/main/java/com/example/be/config/SwaggerConfig.java
+++ be/src/main/java/com/example/be/config/SwaggerConfig.java
@@ -1,4 +1,47 @@
 package com.example.be.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+  @Bean
+  public OpenAPI openAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("TICKETBLITZ API")
+            .version("1.0")
+            .description("콘서트 티켓팅 서비스 API"));
+  }
+
+  // Auth 도메인
+  @Bean
+  public GroupedOpenApi authApi() {
+    return GroupedOpenApi.builder()
+        .group("1. Auth")
+        .pathsToMatch("/auth/**")
+        .build();
+  }
+
+  // User 도메인
+  @Bean
+  public GroupedOpenApi userApi() {
+    return GroupedOpenApi.builder()
+        .group("2. User")
+        .pathsToMatch("/users/**")
+        .build();
+  }
+
+  // Concert 도메인
+  @Bean
+  public GroupedOpenApi concertApi() {
+    return GroupedOpenApi.builder()
+        .group("3. Concert")
+        .pathsToMatch("/concerts/**")
+        .build();
+  }
 }
