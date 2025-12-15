12ab1384de4902d57b82e85f6d1a079cd7b96ae3
sunJ0120 <sspure1214@gmail.com>
2025-12-15 17:50:26 +0900

    fix: logout 설정을 위해 SecuritySchema 추가

--- src/main/java/com/example/be/config/SwaggerConfig.java
+++ src/main/java/com/example/be/config/SwaggerConfig.java
@@ -1,11 +1,19 @@
 package com.example.be.config;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@SecurityScheme(
+    name = "BearerAuth",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
 @Configuration
 public class SwaggerConfig {
 
