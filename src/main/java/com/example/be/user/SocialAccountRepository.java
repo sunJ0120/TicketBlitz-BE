86098697b44d2f734448a45b1e92265a7dcb86a4
sunJ0120 <sspure1214@gmail.com>
2025-12-18 18:21:08 +0900

    feat: socialAccount 레포지토리 추가

--- src/main/java/com/example/be/user/SocialAccountRepository.java
+++ src/main/java/com/example/be/user/SocialAccountRepository.java
@@ -1,4 +1,9 @@
 package com.example.be.user;
 
-public interface SocialAccountRepository {
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SocialAccountRepository extends JpaRepository<SocialAccount, Long> {
+
+  Optional<SocialAccount> findByProviderAndProviderId(Provider provider, String providerId);
 }
