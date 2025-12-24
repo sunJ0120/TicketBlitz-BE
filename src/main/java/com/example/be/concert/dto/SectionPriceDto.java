99ea230d78fdf80a123f68a7bfba3141c5f391ca
sunJ0120 <sspure1214@gmail.com>
2025-12-24 14:12:03 +0900

    feat: 상세 페이지 응답용 dto 추가

--- src/main/java/com/example/be/concert/dto/SectionPriceDto.java
+++ src/main/java/com/example/be/concert/dto/SectionPriceDto.java
@@ -1,4 +1,10 @@
 package com.example.be.concert.dto;
 
-public class SectionPriceDto {
+import com.example.be.concert.domain.ConcertSection;
+
+public record SectionPriceDto(String sectionName, int price) {
+
+  public static SectionPriceDto from(ConcertSection section) {
+    return new SectionPriceDto(section.getSectionLabel().getDisplayName(), section.getPrice());
+  }
 }
