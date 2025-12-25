460300c0b014f1db17dcaba53aa6dec42efdc0d7
sunJ0120 <sspure1214@gmail.com>
2025-12-25 17:36:21 +0900

    feat: 커서 형식 틀렸을때 발생하는 exception 추가

--- src/main/java/com/example/be/common/exception/InvalidCursorException.java
+++ src/main/java/com/example/be/common/exception/InvalidCursorException.java
@@ -1,10 +1,14 @@
-package com.example.be.concert.exception;
+package com.example.be.common.exception;
 
-import com.example.be.common.exception.BusinessException;
+import static com.example.be.common.exception.GlobalErrorCode.INVALID_CURSOR;
 
 public class InvalidCursorException extends BusinessException {
 
-  public InvalidCursorException(String message) {
-    super(message);
+  public InvalidCursorException() {
+    super(INVALID_CURSOR);
+  }
+
+  public InvalidCursorException(String value) {
+    super(GlobalErrorCode.INVALID_CURSOR, "잘못된 커서 값입니다: " + value);
   }
 }
