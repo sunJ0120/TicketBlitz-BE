beab4988ddf93a3adf35db501f7bba3acf92aff7
sunJ0120 <sspure1214@gmail.com>
2025-12-25 17:43:55 +0900

    feat: 더미 데이터 삽입을 위해 repo 구현

--- src/main/java/com/example/be/concert/repository/HallTemplateRepository.java
+++ src/main/java/com/example/be/concert/repository/HallTemplateRepository.java
@@ -3,4 +3,4 @@ package com.example.be.concert.repository;
 import com.example.be.concert.domain.HallTemplate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HallTemplateRepository extends JpaRepository<HallTemplate, Integer> {}
+public interface HallTemplateRepository extends JpaRepository<HallTemplate, Long> {}
