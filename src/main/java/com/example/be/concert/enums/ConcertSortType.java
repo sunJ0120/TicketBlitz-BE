fd3707c2eedca2f51af9d9bf5ab274b434f3ce02
sunJ0120 <sspure1214@gmail.com>
2025-12-25 17:41:34 +0900

    feat: 정렬 조건 관리를 위해 type enum 추가

--- src/main/java/com/example/be/concert/enums/ConcertSortType.java
+++ src/main/java/com/example/be/concert/enums/ConcertSortType.java
@@ -1,4 +1,24 @@
 package com.example.be.concert.enums;
 
+import com.example.be.common.exception.InvalidCursorException;
+import java.util.Arrays;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ConcertSortType {
+  VIEW_COUNT("viewCount"),
+  PRICE("price"),
+  TICKET_OPEN_AT("ticketOpenAt"),
+  CONCERT_DATE("concertDate");
+
+  private final String value;
+
+  public static ConcertSortType from(String value) {
+    return Arrays.stream(values())
+        .filter(type -> type.value.equals(value))
+        .findFirst()
+        .orElseThrow(() -> new InvalidCursorException(value));
+  }
 }
