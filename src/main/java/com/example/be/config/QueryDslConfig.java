b465403164bfc79a15310a28dec7891acac8c6d3
sunJ0120 <sspure1214@gmail.com>
2025-12-25 23:28:31 +0900

    chore: 코드 포멧팅

--- src/main/java/com/example/be/config/QueryDslConfig.java
+++ src/main/java/com/example/be/config/QueryDslConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class QuerydslConfig {
+public class QueryDslConfig {
 
   @PersistenceContext private EntityManager em;
 
