1909c438295517daacd144f17a14d8827b5f9d7a
sunJ0120 <sspure1214@gmail.com>
2025-12-26 15:02:57 +0900

    refactor: 반복되는 로직 하나로 묶어서 정리, 정렬을 위해 enum에 field 정의

--- src/main/java/com/example/be/concert/enums/ConcertSortType.java
+++ src/main/java/com/example/be/concert/enums/ConcertSortType.java
@@ -1,6 +1,9 @@
 package com.example.be.concert.enums;
 
+import static com.example.be.concert.domain.QConcert.concert;
+
 import com.example.be.common.exception.InvalidCursorException;
+import com.querydsl.core.types.dsl.ComparableExpressionBase;
 import java.util.Arrays;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -8,12 +11,13 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ConcertSortType {
-  VIEW_COUNT("viewCount"),
-  PRICE("price"),
-  TICKET_OPEN_AT("ticketOpenAt"),
-  CONCERT_DATE("concertDate");
+  VIEW_COUNT("viewCount", concert.viewCount),
+  PRICE("price", concert.minPrice),
+  TICKET_OPEN_AT("ticketOpenAt", concert.bookingStartAt),
+  CONCERT_DATE("concertDate", concert.startDate);
 
   private final String value;
+  private final ComparableExpressionBase<?> field;
 
   public static ConcertSortType from(String value) {
     return Arrays.stream(values())
