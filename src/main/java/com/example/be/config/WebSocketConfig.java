476f4e8c034166ca37fcb4c87a0635c019aabd34
sunJ0120 <sspure1214@gmail.com>
2025-12-31 22:44:22 +0900

    feat: 웹 소켓 설정 및 연동 url 정의 완료

--- src/main/java/com/example/be/config/WebSocketConfig.java
+++ src/main/java/com/example/be/config/WebSocketConfig.java
@@ -1,4 +1,34 @@
 package com.example.be.config;
 
-public class WebSocket {
+import com.example.be.websocket.interceptor.WebSocketAuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+  private final WebSocketAuthInterceptor webSocketAuthInterceptor;
+
+  @Override
+  public void registerStompEndpoints(StompEndpointRegistry registry) {
+    registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+  }
+
+  @Override
+  public void configureMessageBroker(MessageBrokerRegistry config) {
+    config.setApplicationDestinationPrefixes("/app"); // 클라이언트 → 서버 (요청)
+    config.enableSimpleBroker("/topic", "/queue"); // 서버 → 클라이언트 (브로드캐스트/개인)
+    config.setUserDestinationPrefix("/user");
+  }
+
+  public void configureClientInboundChannel(ChannelRegistration registration) {
+    registration.interceptors(webSocketAuthInterceptor);
+  }
 }
