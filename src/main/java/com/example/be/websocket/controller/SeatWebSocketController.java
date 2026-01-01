4ba60df347b8c53c460b0b01b1e9d60cd27e1a11
sunJ0120 <sspure1214@gmail.com>
2026-01-01 10:15:28 +0900

    feat: 웹소켓 통신 및 로직 처리를 위한 controller 마련

--- src/main/java/com/example/be/websocket/controller/SeatWebSocketController.java
+++ src/main/java/com/example/be/websocket/controller/SeatWebSocketController.java
@@ -1,4 +1,37 @@
 package com.example.be.websocket.controller;
 
+import com.example.be.websocket.dto.SeatHoldRequest;
+import com.example.be.websocket.dto.SeatReleaseRequest;
+import com.example.be.websocket.dto.SeatResponse;
+import java.security.Principal;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
 public class SeatWebSocketController {
+
+  @MessageMapping("/seats/hold")
+  @SendToUser("/queue/seat-result")
+  public SeatResponse holdSeat(SeatHoldRequest request, Principal principal) {
+    Long userId = Long.parseLong(principal.getName());
+    log.info("좌석 점유 요청 - userId = {}, seatId = {}", userId, request.seatId());
+
+    // TODO : Redis를 활용한 좌석 점유 상태 관리하는 로직 필요
+    return null;
+  }
+
+  @MessageMapping("/seats/release")
+  @SendToUser("/queue/seat-result")
+  public SeatResponse releaseSeat(SeatReleaseRequest request, Principal principal) {
+    Long userId = Long.parseLong(principal.getName());
+    log.info("좌석 해제 요청 - userId = {}, seatId = {}", userId, request.seatId());
+
+    // TODO : Redis를 활용한 좌석 점유 해제 상태 관리하는 로직 필요
+    return null;
+  }
 }
